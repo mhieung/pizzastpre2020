9dc8b543e34578b1305e075172a1762136432d69
mhieung <mhieu.nguyen2k@gmail.com>
2020-12-24 14:18:43 +0700

    hieu

--- easy accomd/dist/assets/js/chat.js
+++
@@ -1,8 +0,0 @@
-function openChat() {
-    document.getElementById("chat-popup").style.display = "block";
-  }
-  
-  function closeChat() {
-    document.getElementById("chat-popup").style.display = "none";
-  }
-  
